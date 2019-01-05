852d8269fc68b1fc0dd83312afabed478ee9c2bd
Gwinplane <maxmarinin@gmail.com>
2019-01-05 03:41:43 +0200

    team section tabs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-$( '#team__positions__item-found' ).on({
-    'mouseover': function() {
-        $('#team__caption').slideDown();
-    },
-    'mouseout': function() {
-        $('#team__caption').slideUp();
-    }
-});
+// $( '.team__positions__item' ).on({
+//     'mouseover': function() {
+//         $('.team__caption').slideDown();
+//     },
+//     'mouseout': function() {
+//         $('.team__caption').slideUp();
+//     }
+// });
+
+$('.js-block-item').click(function() {
+	$('.js-block').siblings('.js-block').removeClass("active-box").slideUp();
+    $('.js-block.' + $(this).attr("id")).addClass("active-box").toggle();
+})
