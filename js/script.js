8767b5ea455997dd14b358ed73aff32dccb671d5
Gwinplane <maxmarinin@gmail.com>
2019-01-13 06:23:10 +0200

    test section slick slider select section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ $('.js-block-item').click(function() {
 	$('.js-block').siblings('.js-block').removeClass("active-box").slideUp();
     $('.js-block.' + $(this).attr("id")).addClass("active-box").toggle();
 })
+
+/*$(document).ready(function(){
+  $('.test__click').slick({
+    setting-name: setting-value
+  });
+});*/
+
+$('.test__click').slick({
+	dots: true,
+	arrows: false,
+});
